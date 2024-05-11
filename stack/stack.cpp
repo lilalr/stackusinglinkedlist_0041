e6e2bf124ad9c8bc7e9116a1265b5bcdfbb70029
lilalr <gurennut@gmail.com>
2024-05-11 11:02:34 +0700

    case 4

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -100,4 +100,7 @@ int main() {
                 cout << "Stack is empty. No top value." << endl;
             }
             break;
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
         

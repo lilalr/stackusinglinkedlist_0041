022801656d8524e9cd1ef0a7ce3f6c5a3820bb0c
lilalr <gurennut@gmail.com>
2024-05-11 11:02:49 +0700

    default

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -103,4 +103,13 @@ int main() {
         case 4:
             cout << "Exiting program." << endl;
             break;
-        
+        default:
+            cout << "Invalid choice. Try again." << endl;
+            break;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}

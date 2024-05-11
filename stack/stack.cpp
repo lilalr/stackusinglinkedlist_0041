cf3b3f3cadc53ef8c1fd5e5ea711e31e48089df3
lilalr <gurennut@gmail.com>
2024-05-11 10:53:55 +0700

    membuat void pop

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -33,4 +33,13 @@ public:
         return value;
     }
 
+    // pop operation: remove the topmost element from the stack
+    void pop() {
+        if (isEmpty()) {
+            cout << "Stack is empty." << endl;
+        }
+        cout << "Popped value: " << top->data << endl;
+        top = top->next; // update the top pointer to the next node
+    }
+
     

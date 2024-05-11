04025ac7d7b69ce5de979760baab32cb78b2eb74
lilalr <gurennut@gmail.com>
2024-05-11 10:52:48 +0700

    menampilkan "push value"

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -29,4 +29,8 @@ public:
         newNode->data = value; // 2. assign value
         newNode->next = top; // 3. set the next pointer of the new node to the current top node
         top = newNode; // 4. update the top pointer to the new node
-        
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+
+    

a902a7203de6d87330b44c74d048aca83801d32b
lilalr <gurennut@gmail.com>
2024-05-11 11:02:21 +0700

    case 3

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -92,4 +92,12 @@ int main() {
                 cout << "Stack is empty. Cannot pop." << endl;
             }
             break;
+        case 3:
+            if (!stack.isEmpty()) {
+                stack.peek(); // get the value of the top element
+            }
+            else {
+                cout << "Stack is empty. No top value." << endl;
+            }
+            break;
         

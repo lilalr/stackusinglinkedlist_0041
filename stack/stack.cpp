d750137cbfe8000332927f7f6088fc17bca7986b
lilalr <gurennut@gmail.com>
2024-05-11 10:51:18 +0700

    3. set the next pointer

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -27,4 +27,5 @@ public:
     int push(int value) {
         Node* newNode = new Node(); // 1. allocate memory for the new node
         newNode->data = value; // 2. assign value
+        newNode->next = top; // 3. set the next pointer of the new node to the current top node
         

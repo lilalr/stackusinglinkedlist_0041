59aec23e07e38a8b2d83c5001eeb9f41bdbbb80d
lilalr <gurennut@gmail.com>
2024-05-11 10:50:18 +0700

    1. alokasi memory

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -25,4 +25,5 @@ public:
 
     // push operation: insert an element onto the top of the stack
     int push(int value) {
+        Node* newNode = new Node(); // 1. allocate memory for the new node
         
